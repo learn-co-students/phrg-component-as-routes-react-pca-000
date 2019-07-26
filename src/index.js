b9665e75d978dc3e55846e1d62761bdad3a29c48
Leila Weiss <leila.weiss@powerhrg.com>
2019-07-26 08:40:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
@@ -12,24 +12,46 @@ class Home extends React.Component {
   }
 }
 
-ReactDOM.render((
-  <Router>
-    <React.Fragment>
-      <Route path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/login" component={Login} />
-    </React.Fragment>
-  </Router>),
-  document.getElementById('root')
-);
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
 
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-};
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+
+const About = () => <h1>This is my about component!</h1>;
 
 const Login = () => {
   return (
@@ -48,3 +70,17 @@ const Login = () => {
     </div>
   );
 };
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </React.Fragment>
+  </Router>),
+  document.getElementById('root')
+);
+
+
